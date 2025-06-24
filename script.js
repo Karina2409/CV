46d6ebe38ffcbb2f90143391472ff32f14e329a3
Karina2409 <karina.siardziuk@mail.ru>
2025-06-24 15:14:03 +0300

    fix: fix theme changing

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-setTheme();
+setThemeHTML();
 
-function setTheme() {
-  const theme = localStorage.getItem('theme') || 'light';
+function setThemeHTML() {
+  const theme = getTheme();
   document.documentElement.setAttribute('data-theme', theme);
+  setButtonThemeText(theme);
 }
 
 function changeTheme() {
-  let theme = localStorage.getItem('theme') || 'light';
-  if (theme === 'dark') {
-    theme = 'light';
-  } else {
-    theme = 'dark';
-  }
-  localStorage.setItem('theme', theme);
+  let theme = getTheme() === 'dark' ? 'light' : 'dark';
+  setTheme(theme);
+  setButtonThemeText(theme);
   document.documentElement.setAttribute('data-theme', theme);
+}
+
+function setButtonThemeText(theme) {
+  const themeButton  = document.querySelector(".theme-button");
+  themeButton.textContent = theme === 'dark' ? 'Light' : 'Dark';
 }
