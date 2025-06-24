5881e4d2ccf083a59754ddb1fa21af522fc3ef98
Karina2409 <karina.siardziuk@mail.ru>
2025-06-24 17:51:22 +0300

    feat: add animated links on github and linkedin

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ function setButtonThemeText(theme) {
   const themeButton  = document.querySelector(".theme-button");
   themeButton.textContent = theme === 'dark' ? 'Light' : 'Dark';
 }
+
+const socials = document.querySelectorAll('.about-me__social');
+
+const amplitudes = 10;
+const basePositions = Array.from(socials).map(el => el.offsetTop);
+let angle = 0;
+
+function animateSocial() {
+  angle += 0.01;
+
+  socials.forEach((social, index) => {
+    const baseTop = basePositions[index];
+    const offsetY = Math.sin(angle + index) * amplitudes;
+
+    social.style.top = `${baseTop + offsetY}px`;
+  });
+
+  requestAnimationFrame(animateSocial);
+}
+
+animateSocial();
